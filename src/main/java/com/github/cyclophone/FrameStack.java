ec81f790a802726f2d44682a456d5d8eb427d164
h908714124 <kraftdurchblumen@gmx.de>
2019-06-19 21:37:43 +0200

    cleanup

--- src/main/java/com/github/cyclophone/FrameStack.java
+++ src/main/java/com/github/cyclophone/FrameStack.java
@@ -8,16 +8,17 @@ final class FrameStack {
   private int[][] frames;
   private int[] frameLenghts;
 
-  // pointer to top of the stack
+  // pointer to the top of the stack
   private int ptr;
 
   // tmp buffer for calculations
   private int[] tmp;
 
   FrameStack(int n) {
-    int maxHeight = (n * (n + 1)) / 2;
-    this.frames = new int[Math.max(maxHeight, 16)][n];
-    this.frameLenghts = new int[Math.max(maxHeight, 16)];
+    // height = ptr + 1 (note we start at height 1)
+    int maxHeight = 1 + (n * (n - 1)) / 2;
+    this.frames = new int[maxHeight][n];
+    this.frameLenghts = new int[maxHeight];
     this.tmp = new int[n];
   }
 
